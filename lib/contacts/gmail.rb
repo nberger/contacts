2c487ad2ee1966161556af281e9a4f9f5e473d4d
nberger <nicoberger@gmail.com>
2011-08-15 00:31:06 -0300

    Do not prefetch contacts on gmail connect

--- lib/contacts/gmail.rb
+++ lib/contacts/gmail.rb
@@ -7,13 +7,23 @@ class Contacts
     CONTACTS_FEED = CONTACTS_SCOPE + 'contacts/default/full/?max-results=1000'
     
     def contacts
+      fetch_contacts unless @contacts
       return @contacts if @contacts
     end
     
     def real_connect
       @client = GData::Client::Contacts.new
       @client.clientlogin(@login, @password, @captcha_token, @captcha_response)
-      
+    rescue GData::Client::AuthorizationError => e
+      raise AuthenticationError, "Username or password are incorrect"
+    rescue GData::Client::CaptchaError => e
+      raise CaptchaError
+    end
+
+  TYPES[:gmail] = Gmail
+
+  protected
+    def fetch_contacts
       feed = @client.get(CONTACTS_FEED).to_xml
       
       @contacts = feed.elements.to_a('entry').collect do |entry|
@@ -24,11 +34,6 @@ class Contacts
         [title, email] unless email.nil?
       end
       @contacts.compact!
-    rescue GData::Client::AuthorizationError => e
-      raise AuthenticationError, "Username or password are incorrect"
-    rescue GData::Client::CaptchaError => e
-      raise CaptchaError
     end
   end
-  TYPES[:gmail] = Gmail
 end
